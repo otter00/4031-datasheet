ae07357116f20b9ba61a8260a46c3aaf4b0b6667
otter00 <ivlevanastia2001@mail.ru>
2023-03-10 20:32:55 +0300

    button added

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 let button = document.querySelector(".login-form__btn");
 let form = document.querySelector('#login__form');
-//let errors = [];
 
 button.addEventListener('click', ()=>{
     window.location.href = 'https://new-pro.guap.ru/';
