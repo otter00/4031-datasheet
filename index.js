0dabf3ffe2cf24741d3f129842e73a7f062d7f29
otter00 <ivlevanastia2001@mail.ru>
2023-04-28 21:38:49 +0300

    pro guap link edited v2.0

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ let button = document.querySelector(".login-form__btn");
 let form = document.querySelector('#login__form');
 
 button.addEventListener('click', ()=>{
-    window.location.href = 'https://sso.guap.ru:8443/realms/master/protocol/openid-connect/auth?state=e661261fb721c57ace51147174a21940&scope=profile%20email&response_type=code&approval_prompt=auto&redirect_uri=https%3A%2F%2Fpro.guap.ru%2Foauth%2Fcallback&client_id=prosuai';
+    window.location.href = 'https://pro.guap.ru/';
 });
